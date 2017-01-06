ec9c8ed49bc7ada81d369d313339c10dba87c756
Terri-azure <hongxuan0377@qq.com>
2017-01-06 16:13:30 +0800

    提取配置信息

---
+++ src/main/java/com/bigdata/etl/util/SqlDwInfo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bigdata.etl.util;
+
+/**
+ *
+ * @author GZETL
+ */
+public interface SqlDwInfo {
+    static String driverName = "com.microsoft.sqlserver.jdbc.SQLServerDriver";//加载驱动程序 
+    static String url = "";
+    static String user = "amsqldwsa";
+    static String password = "";
+}
